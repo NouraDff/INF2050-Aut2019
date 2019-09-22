d05522bdbccf3a07218bc5e6d2fef194219db8e4
Noura Djaffri <nouradjaffri@gmail.com>
2019-09-22 12:43:30 -0400

    Ajoute une ligne de commentaire

--- lab1/FizzBuzz.java
+++ lab1/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
             }else if(i % 3 == 0){
                 System.out.println("Fizz");
             }else if( i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println("Buzz");//Ajoute un conflit
             }
         }
     }
