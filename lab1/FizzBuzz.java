40ba3069c642909aee2b3ce2eec16bea1c36854d
Noura Djaffri <nouradjaffri@gmail.com>
2019-09-22 12:42:09 -0400

    Revert "Ajouter else pour aucun des cas"
    This reverts commit 9dc94f143c0e8c0c0b6a6d93f631ac0eef1ef1b1.

--- lab1/FizzBuzz.java
+++ lab1/FizzBuzz.java
@@ -8,8 +8,6 @@ public class FizzBuzz {
                 System.out.println("Fizz");
             }else if( i % 5 == 0) {
                 System.out.println("Buzz");
-            }else {
-                System.out.println("Aucun des cas");
             }
         }
     }
